1d66aab6ca7be11fa42c6045492bc194ff31ae32
Alireza Ghafouri <alirezaghafouri98@gmail.com>
2021-02-07 11:15:58 +0330

    read marking vector

--- sdf.py
+++ sdf.py
@@ -1,7 +1,9 @@
 def read_file():
     file= open ("topology_matrix.txt","rt")
     matrix= [[int(num) for num in line.split(",")] for line in file.readlines()]
-    #return matrix
+    file= open ("marking_vector.txt" , "rt")
+    marking= [int(line) for line in file.readlines()]
+    #return matrix , marking
     
 
 
